751e54a8d06263777f5f1229eb04d4aac462501e
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-02-02 14:01:56 +0700

    added rid to upvoter and downvoter identefication

--- pages/api/v1/post/downvote.js
+++ pages/api/v1/post/downvote.js
@@ -2,13 +2,15 @@ import nextConnect from "next-connect";
 import middleware from "../../../../lib/db";
 import requestIp from "request-ip";
 import { checkToken } from "../../../../lib/csrf";
+import { Cookie } from "next-cookie";
+import { randomBytes } from "crypto";
 
 const handler = nextConnect();
 
 handler.use(middleware);
 
 handler.post(async (req, res) => {
-	if (!checkToken(req)) return res.status(401).send();
+	if (!checkToken(req)) return res.status(403).send();
 
 	if (req.method !== "POST") {
 		res.status(400).json({
@@ -17,6 +19,9 @@ handler.post(async (req, res) => {
 		return;
 	}
 
+	const cookie = Cookie.fromApiRoute(req, res);
+	var rId = cookie.get("rId");
+	var setNewRid = false;
 	const id = parseInt(req.body.id);
 	const detectedIp = requestIp.getClientIp(req);
 
@@ -30,41 +35,56 @@ handler.post(async (req, res) => {
 			message: "Post not found",
 		});
 	} else {
+		if (!rId) {
+			rId = randomBytes(12).toString("hex");
+			cookie.set("rId", rId);
+			setNewRid = true;
+		}
+
+		const downvoter = detectedIp + " " + rId;
 		let postUpvoter = post[0].upvoter;
 		let postDownvoter = post[0].downvoter;
 
 		// if the user has upvoted before
-		if (postUpvoter.includes(detectedIp)) {
+		if (postUpvoter.includes(downvoter)) {
 			// decrement upvote and remove ip from upvoter array
 			// prettier-ignore
 			await req.db.collection("post").updateOne(
                 { id: id },
-                { $inc: { upvote: -1 }, $pull: { upvoter: detectedIp } }
+                { $inc: { upvote: -1 }, $pull: { upvoter: downvoter } }
             );
 		}
 
+		var message = "";
 		// if the user has downvoted before
-		if (postDownvoter.includes(detectedIp)) {
+		if (postDownvoter.includes(downvoter)) {
 			// decrement downvote and remove ip from downvoter array
 			// prettier-ignore
 			await req.db.collection("post").updateOne(
                 { id: id },
-                { $inc: { downvote: -1 }, $pull: { downvoter: detectedIp } }
+                { $inc: { downvote: -1 }, $pull: { downvoter: downvoter } }
             );
 
-			res.status(200).json({
-				message: "Downvote removed",
-			});
+			message = "Downvote removed";
 		} else {
 			// increase downvote and add ip to downvoter array
 			// prettier-ignore
 			await req.db.collection("post").updateOne(
                 { id: id },
-                { $inc: { downvote: 1 }, $push: { downvoter: detectedIp } },
+                { $inc: { downvote: 1 }, $push: { downvoter: downvoter } },
             );
 
+			message = "Downvoted";
+		}
+
+		if (setNewRid) {
 			res.status(200).json({
-				message: "Downvoted",
+				message: message,
+				cookieValue: cookie.get("rId"),
+			});
+		} else {
+			res.status(200).json({
+				message: message,
 			});
 		}
 	}
--- pages/api/v1/post/upvote.js
+++ pages/api/v1/post/upvote.js
@@ -2,6 +2,8 @@ import nextConnect from "next-connect";
 import middleware from "../../../../lib/db";
 import requestIp from "request-ip";
 import { checkToken } from "../../../../lib/csrf";
+import { Cookie } from "next-cookie";
+import { randomBytes } from "crypto";
 
 const handler = nextConnect();
 
@@ -17,7 +19,11 @@ handler.post(async (req, res) => {
 		return;
 	}
 
-	const id = parseInt(req.body.id);
+	const cookie = Cookie.fromApiRoute(req, res);
+	var rId = cookie.get("rId");
+	var setNewRid = false;
+	// const id = parseInt(req.body.id);
+	const id = parseInt(req.query.id);
 	const detectedIp = requestIp.getClientIp(req);
 
 	// prettier-ignore
@@ -30,40 +36,55 @@ handler.post(async (req, res) => {
 			message: "Post not found",
 		});
 	} else {
+		if (!rId) {
+			rId = randomBytes(12).toString("hex");
+			cookie.set("rId", rId);
+			setNewRid = true;
+		}
+
+		const upvoter = detectedIp + " " + rId;
 		let postUpvoter = post[0].upvoter;
 		let postDownvoter = post[0].downvoter;
 
 		// if the user has downvoted before
-		if (postDownvoter.includes(detectedIp)) {
+		if (postDownvoter.includes(upvoter)) {
 			// decrement downvote and remove ip from downvoter array
 			// prettier-ignore
 			await req.db.collection("post").updateOne(
 				{ id: id },
-				{ $inc: { downvote: -1 }, $pull: { downvoter: detectedIp } }
+				{ $inc: { downvote: -1 }, $pull: { downvoter: upvoter } }
 			);
 		}
 
+		var message = "";
 		// if the user has upvoted before
-		if (postUpvoter.includes(detectedIp)) {
+		if (postUpvoter.includes(upvoter)) {
 			// decrement upvote and remove ip from upvoter array
 			// prettier-ignore
 			await req.db.collection("post").updateOne(
 				{ id: id },
-				{ $inc: { upvote: -1 }, $pull: { upvoter: detectedIp } }
+				{ $inc: { upvote: -1 }, $pull: { upvoter: upvoter } }
 			);
-			res.status(200).json({
-				message: "Upvote removed",
-			});
+			message = "Upvote removed";
 		} else {
 			// increase upvote and add ip to upvoter array
 			// prettier-ignore
 			await req.db.collection("post").updateOne(
 				{ id: id },
-				{ $inc: { upvote: 1 }, $push: { upvoter: detectedIp } },
+				{ $inc: { upvote: 1 }, $push: { upvoter: upvoter } },
 			);
 
+			message = "Upvoted";
+		}
+
+		if (setNewRid) {
 			res.status(200).json({
-				message: "Upvoted",
+				message: message,
+				cookieValue: cookie.get("rId"),
+			});
+		} else {
+			res.status(200).json({
+				message: message,
 			});
 		}
 	}
