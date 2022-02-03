0e0130d3df04c6dca3e21871eefcd5e20a663768
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-02-03 13:41:38 +0700

    darkmode-toggler-icon

--- components/theme-switcher/DarkModeToggle.jsx
+++ components/theme-switcher/DarkModeToggle.jsx
@@ -86,12 +86,5 @@ export default function DarkModeToggle() {
 		}
 	}, []);
 
-	return (
-		<div className='form-check form-switch ms-auto mt-3 me-3 darkmode-toggler'>
-			<label className='form-check-label ms-3' htmlFor='lightSwitch' style={{ marginLeft: "3px" }}>
-				<Image src={logo} className='bi bi-brightness-high' width={"25"} height={"25"} fill='white' alt='light/dark toggler' viewBox='0 0 16 16' style={{ marginBottom: "7px", cursor: "pointer" }} />
-			</label>
-			<input className='form-check-input' type={"checkbox"} id='lightSwitch' onChange={toggleBgMode} checked={checked} style={{ marginLeft: "0px", cursor: "pointer" }} />
-		</div>
-	);
+	return <Image src={logo} className='bi bi-brightness-high darkmode-toggler' width={"25"} height={"25"} fill='white' alt='light/dark toggler' viewBox='0 0 16 16' onClick={toggleBgMode} />;
 }
