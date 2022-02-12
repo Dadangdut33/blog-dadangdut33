5cbade0a7798754fb9d66714b7168b41f76e6bf3
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-02-12 12:36:41 +0700

    fix thumbnail inside

--- pages/r/[id]/[title].jsx
+++ pages/r/[id]/[title].jsx
@@ -157,9 +157,7 @@ export default function postIdWithTitle({ post, cookie, admin }) {
 						<i className='fas fa-home'></i>
 					</a>
 					<div className='title'>
-						<div className={"image-container md-thumbnail"}>
-							<Image src={post.thumbnail} alt={post.title + "thumbnail"} layout='fill' className={"image"} />
-						</div>
+						<Image src={post.thumbnail} alt={post.title + "thumbnail"} width={1000} height={500} />
 						<h1 id={post.title.replace(/\s+/g, "-")}>{post.title}</h1>
 						<div className='post-stats'>
 							<p className='text-muted first'>
